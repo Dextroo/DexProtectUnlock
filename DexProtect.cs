dee00fc5bb26cd6d4bc688eb91cddd107781579e
Dextroo <88213329+Dextroo@users.noreply.github.com>
2022-07-19 20:25:29 -0400

    Possible polygon explosion fix
    Attempts to fix the remote "polygon explosion" by forcing a Reset Avatar immediately after switching into a protected avatar. Parameter state is still preserved.

--- DexProtect.cs
+++ DexProtect.cs
@@ -24,9 +24,10 @@ namespace DexProtect
         public static List<bool> lastVals = new List<bool>() { false, false, false, true };
         public static List<string> roundParams;
         public static bool inLockedAvatar;
-        public static bool refreshParamKeys;
+        public static int triggerReset = 0;
         public static Il2CppSystem.Collections.Generic.Dictionary<int, AvatarParameter> allParams;
         public static Dictionary<string, int> paramKeys;
+        public static List<(string, float)> storedParams;
         private static void OnAvatarInstantiate(Player player, GameObject avatar, VRC_AvatarDescriptor descriptor)
         {
             try
@@ -37,8 +38,18 @@ namespace DexProtect
                     if (File.Exists("UserData/DexProtect/" + id + ".key"))
                     {
                         log.Msg("Key detected for avatar ID " + id + ", unlocking...");
+                        var keyString = File.ReadAllText("UserData/DexProtect/" + id + ".key", Encoding.Unicode).Split('|').ToList();
+                        keyParams = keyString.Skip(5).Select(x => new string(x.Skip(1).ToArray())).ToList();
+                        keyBools = keyString.Skip(5).Select(x => x[0] == '1').ToList();
+                        roundParams = keyString.GetRange(1, 4);
+                        var roundSplit = keyString[0];
+                        playerAnim = player._vrcplayer.field_Private_AnimatorControllerManager_0.field_Private_AvatarAnimParamController_0.field_Private_AvatarPlayableController_0;
+                        playerFX = playerAnim.field_Private_AvatarAnimLayer_7.field_Private_AnimatorControllerPlayable_0;
+                        allParams = playerAnim.field_Private_Dictionary_2_Int32_AvatarParameter_0;
+                        SetParams();
+                        playerFX.SetBool(roundSplit, true);
+                        if (triggerReset == 2) triggerReset = 0;
                         inLockedAvatar = true;
-                        refreshParamKeys = true;
                     }
                     else
                     {
@@ -46,16 +57,6 @@ namespace DexProtect
                         inLockedAvatar = false;
                         return;
                     }
-                    var keyString = File.ReadAllText("UserData/DexProtect/" + id + ".key", Encoding.Unicode).Split('|').ToList();
-                    keyParams = keyString.Skip(5).Select(x => new string(x.Skip(1).ToArray())).ToList();
-                    keyBools = keyString.Skip(5).Select(x => x[0] == '1').ToList();
-                    roundParams = keyString.GetRange(1, 4);
-                    var roundSplit = keyString[0];
-                    playerAnim = player._vrcplayer.field_Private_AnimatorControllerManager_0.field_Private_AvatarAnimParamController_0.field_Private_AvatarPlayableController_0;
-                    playerFX = playerAnim.field_Private_AvatarAnimLayer_7.field_Private_AnimatorControllerPlayable_0;
-                    allParams = playerAnim.field_Private_Dictionary_2_Int32_AvatarParameter_0;
-                    SetParams();
-                    playerFX.SetBool(roundSplit, true);
                 }
             }
             catch { }
@@ -68,14 +69,7 @@ namespace DexProtect
                 var ind = keyParams.IndexOf(param.name);
                 if (ind != -1)
                 {
-                    if (keyBools[ind])
-                    {
-                        playerFX.SetBool(param.name, true);
-                    }
-                    else
-                    {
-                        playerFX.SetBool(param.name, false);
-                    }
+                    playerFX.SetBool(param.name, keyBools[ind]);
                 }
             }
         }
@@ -83,41 +77,54 @@ namespace DexProtect
         {
             for (var i = 0; i < keyParams.Count; i++)
             {
-                if (paramKeys.ContainsKey(keyParams[i]))
-                {
-                    if (keyBools[i])
-                    {
-                        allParams[paramKeys[keyParams[i]]].prop_Int32_1 = 1;
-                    }
-                    else
-                    {
-                        allParams[paramKeys[keyParams[i]]].prop_Int32_1 = 0;
-                    }
-                }
+                allParams[paramKeys[keyParams[i]]].prop_Int32_1 = Convert.ToInt32(keyBools[i]);
+            }
+        }
+        private static void StoreParams()
+        {
+            storedParams = new List<(string, float)>();
+            foreach (var param in allParams)
+            {
+                storedParams.Add((param.Value.field_Private_String_0, param.Value.prop_Single_0));
+            }
+        }
+        private static void RestoreParams()
+        {
+            foreach (var storedParam in storedParams)
+            {
+                allParams[paramKeys[storedParam.Item1]].prop_Single_0 = storedParam.Item2;
             }
         }
         public override void OnUpdate()
         {
             if (!inLockedAvatar) return;
-            try
+            if (playerAnim == null) return;
+            var currentVals = new List<bool>();
+            for (var i = 0; i < 4; i++) currentVals.Add(playerFX.GetBool(roundParams[i]));
+            if (currentVals[0] == roundVals[0] && currentVals[1] == roundVals[1] && currentVals[2] == roundVals[2] && currentVals[3] == roundVals[3] &&
+                (currentVals[0] != lastVals[0] || currentVals[1] != lastVals[1] || currentVals[2] != lastVals[2] || currentVals[3] != lastVals[3]) &&
+                !(lastVals[0] == false && lastVals[1] == false && lastVals[2] == false && lastVals[3] == false))
             {
-                var currentVals = new List<bool>();
-                for (var i = 0; i < 4; i++) currentVals.Add(playerFX.GetBool(roundParams[i]));
-                if (currentVals[0] == roundVals[0] && currentVals[1] == roundVals[1] && currentVals[2] == roundVals[2] && currentVals[3] == roundVals[3] &&
-                    (currentVals[0] != lastVals[0] || currentVals[1] != lastVals[1] || currentVals[2] != lastVals[2] || currentVals[3] != lastVals[3]) &&
-                    !(lastVals[0] == false && lastVals[1] == false && lastVals[2] == false && lastVals[3] == false))
+                if (triggerReset != 2)
                 {
-                    if (refreshParamKeys)
+                    paramKeys = new Dictionary<string, int>();
+                    foreach (var entry in allParams) paramKeys.Add(entry.Value.field_Private_String_0, entry.Key);
+                    if (triggerReset == 0)
                     {
-                        refreshParamKeys = false;
-                        paramKeys = new Dictionary<string, int>();
-                        foreach (var entry in allParams) paramKeys.Add(entry.Value.field_Private_String_0, entry.Key);
+                        triggerReset = 1;
+                        StoreParams();
+                        ResetAvatar();
+                        return;
                     }
-                    SetParamsLate();
+                    else
+                    {
+                        triggerReset = 2;
+                        RestoreParams();
+                    }
                 }
-                lastVals = currentVals;
+                SetParamsLate();
             }
-            catch { }
+            lastVals = currentVals;
         }
         public override void OnApplicationStart()
         {
@@ -126,6 +133,6 @@ namespace DexProtect
                 VRCAvatarManager.field_Private_Static_Action_3_Player_GameObject_VRC_AvatarDescriptor_0
             ).Cast<Il2CppSystem.Action<Player, GameObject, VRC_AvatarDescriptor>>();
         }
-        
+        private static void ResetAvatar() => VRCPlayer.field_Internal_Static_VRCPlayer_0.Method_Public_Void_PDM_1();
     }
 }
